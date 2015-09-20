fbf6e8e3a2ed51eb5a76f2cc8cb922df2e5297e1
Jon Bronson <jonbronson@gmail.com>
2015-09-19 23:50:06 -0400

    fixing indentation

--- js/cleavermesher.js
+++ js/cleavermesher.js
@@ -32,183 +32,183 @@ var stencilTable = [[_ABC, _A, _AB],
 var materialTable = [_A, _B, _B, _C, _C, _A];
 
 var CleaverMesher = function() {
-	this.alpha = 0.3;  
+  this.alpha = 0.3;
 };
 
 CleaverMesher.prototype.setInputFields = function(inputFields) {
-	this.fields = inputFields;
+  this.fields = inputFields;
 };
 
 CleaverMesher.prototype.setInputMesh = function(inputMesh) {
-	this.mesh = inputMesh;
+  this.mesh = inputMesh;
 };
 
-CleaverMesher.prototype.materialAt_ = function(x, y) {	
-	var max_material = 0;
-	var max_value = -100000;  // todo replace with constant
-	for (var m=0; m < this.fields.length; m++) {		
-		var value = this.fields[m].valueAt(x, y);
-		if (value > max_value) {
-			max_material = m;
-			max_value = value;
-		}
-	}
+CleaverMesher.prototype.materialAt_ = function(x, y) {  
+  var max_material = 0;
+  var max_value = -100000;  // todo replace with constant
+  for (var m=0; m < this.fields.length; m++) {    
+    var value = this.fields[m].valueAt(x, y);
+    if (value > max_value) {
+      max_material = m;
+      max_value = value;
+    }
+  }
 
-	return max_material;
+  return max_material;
 };
 
 CleaverMesher.prototype.sampleFields = function() {
-	for (var i=0; i < this.mesh.verts.length; i++) {		
-		var m = this.materialAt_(this.mesh.verts[i].pos.x, this.mesh.verts[i].pos.y);		
-		this.mesh.verts[i].material = m;
-	}
+  for (var i=0; i < this.mesh.verts.length; i++) {    
+    var m = this.materialAt_(this.mesh.verts[i].pos.x, this.mesh.verts[i].pos.y);   
+    this.mesh.verts[i].material = m;
+  }
 };
 
 CleaverMesher.prototype.computeCutForEdge_ = function(edge) {
-	var v1 = edge.vertex;
-	var v2 = edge.mate.vertex;
+  var v1 = edge.vertex;
+  var v2 = edge.mate.vertex;
 
-	edge.evaluated = true;
-	edge.mate.evaluated = true;
+  edge.evaluated = true;
+  edge.mate.evaluated = true;
 
-	if (v1.material == v2.material) {
-		return;
-	}
+  if (v1.material == v2.material) {
+    return;
+  }
 
-	var aMaterial = v1.material;
-	var bMaterial = v2.material;
+  var aMaterial = v1.material;
+  var bMaterial = v2.material;
 
-	var a1 = this.fields[aMaterial].valueAt(v1.pos.x, v1.pos.y);
-	var a2 = this.fields[aMaterial].valueAt(v2.pos.x, v2.pos.y);
-	var b1 = this.fields[bMaterial].valueAt(v1.pos.x, v1.pos.y);
-	var b2 = this.fields[bMaterial].valueAt(v2.pos.x, v2.pos.y);
-	var top = (a1 - b1);
-	var bot = (b2 - a2 + a1 - b1);
-	var t = top / bot;
-	t = Math.max(t, 0.0);
-	t = Math.min(t, 1.0);
-	var cx = v1.pos.x*(1-t) + v2.pos.x*t;
-	var cy = v1.pos.y*(1-t) + v2.pos.y*t;
-	
-	var cut = new Vertex(new Vector(cx, cy));
-	cut.order_ = 1;
-	edge.cut = cut;
-	edge.mate.cut = cut;
+  var a1 = this.fields[aMaterial].valueAt(v1.pos.x, v1.pos.y);
+  var a2 = this.fields[aMaterial].valueAt(v2.pos.x, v2.pos.y);
+  var b1 = this.fields[bMaterial].valueAt(v1.pos.x, v1.pos.y);
+  var b2 = this.fields[bMaterial].valueAt(v2.pos.x, v2.pos.y);
+  var top = (a1 - b1);
+  var bot = (b2 - a2 + a1 - b1);
+  var t = top / bot;
+  t = Math.max(t, 0.0);
+  t = Math.min(t, 1.0);
+  var cx = v1.pos.x*(1-t) + v2.pos.x*t;
+  var cy = v1.pos.y*(1-t) + v2.pos.y*t;
+  
+  var cut = new Vertex(new Vector(cx, cy));
+  cut.order_ = 1;
+  edge.cut = cut;
+  edge.mate.cut = cut;
 
-	if (t < 0.5)
-		cut.closestGeometry = v1;
-	else
-		cut.closestGeometry = v2;
+  if (t < 0.5)
+    cut.closestGeometry = v1;
+  else
+    cut.closestGeometry = v2;
 
-	// check violating condition
-	if (t <= this.alpha || t >= (1 - this.alpha))
-		cut.violating = true;
-	else
-		cut.violating = false;
-	
-	return cut;
+  // check violating condition
+  if (t <= this.alpha || t >= (1 - this.alpha))
+    cut.violating = true;
+  else
+    cut.violating = false;
+  
+  return cut;
 };
 
 CleaverMesher.prototype.computeCuts_ = function() {
-	var cuts = [];
-	for (e in this.mesh.halfEdges) {
-		var edge = this.mesh.halfEdges[e];
-		if (!edge.evaluated) {
-			var cut = this.computeCutForEdge_(edge);
-			if (cut) {
-				cuts.push(cut);
-			}
-		}
-	}
-	return cuts;
+  var cuts = [];
+  for (e in this.mesh.halfEdges) {
+    var edge = this.mesh.halfEdges[e];
+    if (!edge.evaluated) {
+      var cut = this.computeCutForEdge_(edge);
+      if (cut) {
+        cuts.push(cut);
+      }
+    }
+  }
+  return cuts;
 };
 
 CleaverMesher.prototype.computeTriples_ = function() {
-	return [];
+  return [];
 };
 
 CleaverMesher.prototype.computeInterfaces = function() {
   this.cuts = this.computeCuts_();
-	this.triples = this.computeTriples_();
+  this.triples = this.computeTriples_();
 };
 
 CleaverMesher.prototype.generalizeTriangles = function() {
-	//--------------------------------------
+  //--------------------------------------
   // Loop over all tets that contain cuts
   //--------------------------------------
   //   (For Now, Looping over ALL tets)
   for (var f=0; f < this.mesh.faces.length; f++) {
-  	var face = this.mesh.faces[f];
-  	var edges = face.halfEdges;
-  	var cut_count = 0;
+    var face = this.mesh.faces[f];
+    var edges = face.halfEdges;
+    var cut_count = 0;
 
-  	//------------------------------
+    //------------------------------
     // if no triple, start generalization
     //------------------------------
     if(face && !face.triple)
     {  
-  		for (var e=0; e < 3; e++) {
-  			cut_count += face.halfEdges[e].cut && face.halfEdges[e].cut.order() == 1 ? 1 : 0;
-  		}  		
+      for (var e=0; e < 3; e++) {
+        cut_count += face.halfEdges[e].cut && face.halfEdges[e].cut.order() == 1 ? 1 : 0;
+      }     
 
-  		// create virtual edge cuts where needed
-  		var virtual_count = 0;
-  		var v_e; 
-  		for (var e=0; e < 3; e++) {
-  			if (!edges[e].cut) {
-  				// always use the smaller id
-  				if (edges[e].vertex.id < edges[e].mate.vertex.id) {
-  					edges[e].cut = edges[e].vertex;
-  				} else {
-  					edges[e].cut = edges[e].mate.vertex;
-  				}
+      // create virtual edge cuts where needed
+      var virtual_count = 0;
+      var v_e; 
+      for (var e=0; e < 3; e++) {
+        if (!edges[e].cut) {
+          // always use the smaller id
+          if (edges[e].vertex.id < edges[e].mate.vertex.id) {
+            edges[e].cut = edges[e].vertex;
+          } else {
+            edges[e].cut = edges[e].mate.vertex;
+          }
 
-  				// copy to mate edge
-  				edges[e].mate.cut = edges[e].cut;
+          // copy to mate edge
+          edges[e].mate.cut = edges[e].cut;
 
-  				v_e = e;
-  				virtual_count++;
-  			} else if(edges[e].cut.order() == 0) {
-  				v_e = e;
-  				virtual_count++;
-  			}
-  		}
+          v_e = e;
+          virtual_count++;
+        } else if(edges[e].cut.order() == 0) {
+          v_e = e;
+          virtual_count++;
+        }
+      }
 
 
 
-			// create virtual triple			
-			switch (virtual_count) {
-				case 0:  
-					throw new Error('Three cuts and no triple.');
-					break;
-				case 1:
-				  // move to edge virtual cut went to
-				  for (var i=0; i < 3; i++) {
-				  	// ignore edge with the virtual cut on it
-				  	if (i == v_e)
-				  		continue;
+      // create virtual triple      
+      switch (virtual_count) {
+        case 0:  
+          throw new Error('Three cuts and no triple.');
+          break;
+        case 1:
+          // move to edge virtual cut went to
+          for (var i=0; i < 3; i++) {
+            // ignore edge with the virtual cut on it
+            if (i == v_e)
+              continue;
 
-				  	if (edges[i].vertex == edges[v_e].cut || edges[i].mate.vertex == edges[v_e].cut) {
+            if (edges[i].vertex == edges[v_e].cut || edges[i].mate.vertex == edges[v_e].cut) {
               face.triple = edges[i].cut;
               break;
             }
-				  }				  
-					break;
-				case 2:  throw new Error('Only one cut on triangle.');
-				case 3:				
-					// move to minimal index vertex	
-					if (face.v1.id < face.v2.id && face.v1.id < face.v3.id)
-						face.triple = face.v1;
-					else if(face.v2.id < face.v1.id && face.v2.id < face.v3.id)
-						face.triple = face.v2;
-					else if(face.v3.id < face.v1.id && face.v3.id < face.v2.id)
-						face.triple = face.v3;
-					else
-						throw new Error('Problem finding minimum id');
-					break;
-				default:
-					throw new Error('Impossible virtual cut count: ' + virtual_count);
-					break;
+          }         
+          break;
+        case 2:  throw new Error('Only one cut on triangle.');
+        case 3:       
+          // move to minimal index vertex 
+          if (face.v1.id < face.v2.id && face.v1.id < face.v3.id)
+            face.triple = face.v1;
+          else if(face.v2.id < face.v1.id && face.v2.id < face.v3.id)
+            face.triple = face.v2;
+          else if(face.v3.id < face.v1.id && face.v3.id < face.v2.id)
+            face.triple = face.v3;
+          else
+            throw new Error('Problem finding minimum id');
+          break;
+        default:
+          throw new Error('Impossible virtual cut count: ' + virtual_count);
+          break;
       }
     }
   }
@@ -216,33 +216,33 @@ CleaverMesher.prototype.generalizeTriangles = function() {
 
 CleaverMesher.prototype.snapAndWarpForVertex = function(vertex) {
 
-	var incident_edges = this.mesh.getEdgesAroundVertex(vertex);
-	var viol_edges = [];
-	var part_edges = [];
-	var viol_faces = [];
-	var part_faces = [];
+  var incident_edges = this.mesh.getEdgesAroundVertex(vertex);
+  var viol_edges = [];
+  var part_edges = [];
+  var viol_faces = [];
+  var part_faces = [];
 
-	for (var e=0; e < incident_edges.length; e++) {
-		var edge = incident_edges[e];
-		if (edge.cut.order() == CUT) {   // Maybe todo replace comparison with isCut() method.  implmementation shouldn't be exposed
-			if (edge.cut.violating && edge.cut.closestGeometry == vertex) {
-				viol_edges.push(edge);
-			} else {
-				part_edges.push(edge);
-			}
-		}	
-	}
+  for (var e=0; e < incident_edges.length; e++) {
+    var edge = incident_edges[e];
+    if (edge.cut.order() == CUT) {   // Maybe todo replace comparison with isCut() method.  implmementation shouldn't be exposed
+      if (edge.cut.violating && edge.cut.closestGeometry == vertex) {
+        viol_edges.push(edge);
+      } else {
+        part_edges.push(edge);
+      }
+    } 
+  }
 
-	// TODO: Add participating and violating triple points.
+  // TODO: Add participating and violating triple points.
 
 
-	//-----------------------------------------
+  //-----------------------------------------
   // If no violations, move to next vertex
   //-----------------------------------------
-	if (viol_edges.length == 0 && viol_faces.length == 0)
-		return;
+  if (viol_edges.length == 0 && viol_faces.length == 0)
+    return;
 
-	//-----------------------------------------
+  //-----------------------------------------
   // Compute Warp Point
   //-----------------------------------------
   var warp_point = Vector.ZERO();
@@ -261,8 +261,8 @@ CleaverMesher.prototype.snapAndWarpForVertex = function(vertex) {
   //------------------------------------------------------
   /*
   for (var e=0; e < part_edges.length; e++) {
-  	var edge = part_edges[e];
-  	var face = this.getInnerFace(edge, vertex, warp_point);
+    var edge = part_edges[e];
+    var face = this.getInnerFace(edge, vertex, warp_point);
   }
   */
 
@@ -284,61 +284,61 @@ CleaverMesher.prototype.snapAndWarpForVertex = function(vertex) {
   // Delete All Violations
   //------------------------
   // 1) cuts
-	for(var e=0; e < viol_edges.length; e++)
+  for(var e=0; e < viol_edges.length; e++)
     this.snapCutForEdgeToVertex(viol_edges[e], vertex);
   for(var e=0; e < part_edges.length; e++)
     this.snapCutForEdgeToVertex(part_edges[e], vertex);
 };
 
 CleaverMesher.prototype.getInnerFace = function(edge, warpVertex, warpPt) {
-	var staticVertex = null
-	if (warpVertex === edge.vertex) {
-		staticVertex = edge.mate.vertex;
-	} else if (warpVertex === edge.mate.vertex) {
-		staticVertex = edge.vertex;
-	} else {
-		throw new Error('warp Edge doesn\'t contain warp vertex.');
-	}
+  var staticVertex = null
+  if (warpVertex === edge.vertex) {
+    staticVertex = edge.mate.vertex;
+  } else if (warpVertex === edge.mate.vertex) {
+    staticVertex = edge.vertex;
+  } else {
+    throw new Error('warp Edge doesn\'t contain warp vertex.');
+  }
 
-	var faces = this.mesh.getFacesAroundEdge(edge);
+  var faces = this.mesh.getFacesAroundEdge(edge);
 
-	var edges = [];
-	for (var f=0; f < faces.length; f++) {
-		for (var e=0; e < 3; e++) {
-		  var edge = faces[f].halfEdges[e];
-		  if (edge.vertex === staticVertex || edge.mate.vertex === staticVertex) {  // todo:  write edge.contains(vertex) method
-		  	continue;
-		  } else {
-		  	edges.push(edge);
-		  }
-		}		
-	}
+  var edges = [];
+  for (var f=0; f < faces.length; f++) {
+    for (var e=0; e < 3; e++) {
+      var edge = faces[f].halfEdges[e];
+      if (edge.vertex === staticVertex || edge.mate.vertex === staticVertex) {  // todo:  write edge.contains(vertex) method
+        continue;
+      } else {
+        edges.push(edge);
+      }
+    }   
+  }
 
-	if (edges.length != faces.length) {
-		throw new Error ('Failed to pair adjacent faces to their intersecting edges');
-	}
+  if (edges.length != faces.length) {
+    throw new Error ('Failed to pair adjacent faces to their intersecting edges');
+  }
 
-	// compute intersection with both edge
-	var intersections = [];
-	for (var e=0; e < edges.length; e++) {
-		var edge = edges[e];
-		var p1,p2,p3,p4;
-		p1 = staticVertex.pos;
-		p2 = warpPt;
-		p3 = warpVertex.pos;
-		p4 = edge.vertex === warpVertex ? edge.mate.vertex.pos : edge.vertex.pos;
-	  var intersection = GeomUtil.computeLineIntersection(p1, p2, p3, p4);
-		intersections.push(intersection);	
-		console.log('intersection t=' + intersection.ub);
+  // compute intersection with both edge
+  var intersections = [];
+  for (var e=0; e < edges.length; e++) {
+    var edge = edges[e];
+    var p1,p2,p3,p4;
+    p1 = staticVertex.pos;
+    p2 = warpPt;
+    p3 = warpVertex.pos;
+    p4 = edge.vertex === warpVertex ? edge.mate.vertex.pos : edge.vertex.pos;
+    var intersection = GeomUtil.computeLineIntersection(p1, p2, p3, p4);
+    intersections.push(intersection); 
+    console.log('intersection t=' + intersection.ub);
   }
 
   var inner = 0;
   var max_ub = 0;
   for (var e=0; e < edges.length; e++) {
-  	if (intersections.ub > max_ub) {
-  		inner = e;
-  		max_ub = intersections.ub;
-  	}
+    if (intersections.ub > max_ub) {
+      inner = e;
+      max_ub = intersections.ub;
+    }
   }
 
   return faces[inner];
@@ -348,17 +348,17 @@ CleaverMesher.prototype.snapCutForEdgeToVertex = function(edge, vertex) {
   if(edge.cut.order_ == CUT)
     edge.cut.parent = vertex;
   else{
-  	console.log('shoudlnt be here');
+    console.log('shoudlnt be here');
     edge.cut = vertex;
     edge.mate.cut = vertex;
   }
 };
 
 CleaverMesher.prototype.snapAndWarpVertexViolations = function() {
-	for (var v=0; v < this.mesh.verts.length; v++) {
-		var vertex = this.mesh.verts[v];
-		this.snapAndWarpForVertex(vertex);
-	}
+  for (var v=0; v < this.mesh.verts.length; v++) {
+    var vertex = this.mesh.verts[v];
+    this.snapAndWarpForVertex(vertex);
+  }
 };
 
 CleaverMesher.prototype.snapAndWarpEdgeViolations = function() {
@@ -366,67 +366,67 @@ CleaverMesher.prototype.snapAndWarpEdgeViolations = function() {
 };
 
 CleaverMesher.prototype.snapAndWarpViolations = function() {
-	this.snapAndWarpVertexViolations();
-	this.snapAndWarpEdgeViolations();
+  this.snapAndWarpVertexViolations();
+  this.snapAndWarpEdgeViolations();
 };
 
 CleaverMesher.prototype.createStencilTriangles = function() {
 
-	var outputCount = 0;
+  var outputCount = 0;
 
-	var numFaces = this.mesh.faces.length;
-	for (var f=0; f < numFaces; f++) {
-		var face = this.mesh.faces[f];
-		var cut_count = 0;
+  var numFaces = this.mesh.faces.length;
+  for (var f=0; f < numFaces; f++) {
+    var face = this.mesh.faces[f];
+    var cut_count = 0;
 
-		
-		for (var e=0; e < 3; e++) {
-			cut_count += face.halfEdges[e].cut.order() == 1 ? 1 : 0;
-		}
-		
-		// TODO: figure out a way to continue here with proper material if
-		//       not stencil to output (which vertex material is correct?)
+    
+    for (var e=0; e < 3; e++) {
+      cut_count += face.halfEdges[e].cut.order() == 1 ? 1 : 0;
+    }
+    
+    // TODO: figure out a way to continue here with proper material if
+    //       not stencil to output (which vertex material is correct?)
 
-		/*
-		if (cut_count == 0) {
-			if(face.v1.material == face.v2.material)
-			face.material = ? face.v1.material : face.v3.material;
-			continue;
-		}
-		*/
-			
+    /*
+    if (cut_count == 0) {
+      if(face.v1.material == face.v2.material)
+      face.material = ? face.v1.material : face.v3.material;
+      continue;
+    }
+    */
+      
 
-		// build vertex list
-		var verts = [face.v1, face.v2, face.v3, 
-						     face.halfEdges[0].cut, face.halfEdges[1].cut,	face.halfEdges[2].cut,
-						     face.triple];
+    // build vertex list
+    var verts = [face.v1, face.v2, face.v3, 
+                 face.halfEdges[0].cut, face.halfEdges[1].cut,  face.halfEdges[2].cut,
+                 face.triple];
 
-	  for(var st=0; st < 6; st++) {  
-    	var v1 = verts[stencilTable[st][0]].root();
-    	var v2 = verts[stencilTable[st][1]].root();
-    	var v3 = verts[stencilTable[st][2]].root();
-    	var vM = verts[materialTable[st]].root();
+    for(var st=0; st < 6; st++) {  
+      var v1 = verts[stencilTable[st][0]].root();
+      var v2 = verts[stencilTable[st][1]].root();
+      var v3 = verts[stencilTable[st][2]].root();
+      var vM = verts[materialTable[st]].root();
 
-    	//----------------------------------------------------------
-    	//  Ensure Triangle Not Degenerate (all vertices must be unique)
-    	//----------------------------------------------------------
-    	if(v1 == v2 || v1 == v3 || v2 == v3)
-      	continue;
+      //----------------------------------------------------------
+      //  Ensure Triangle Not Degenerate (all vertices must be unique)
+      //----------------------------------------------------------
+      if(v1 == v2 || v1 == v3 || v2 == v3)
+        continue;
 
       this.mesh.createFace(v1, v2, v3, vM.material);
       outputCount++;
-	  }		
-	}
-	console.log('Input mesh has ' + numFaces + ' triangles.');
-	console.log('Total of ' + outputCount + ' new triangles created');
-};	
+    }   
+  }
+  console.log('Input mesh has ' + numFaces + ' triangles.');
+  console.log('Total of ' + outputCount + ' new triangles created');
+};  
 
 CleaverMesher.prototype.cleave = function() {
-	this.sampleFields();
-	this.computeInterfaces();
-	this.generalizeTriangles();
-	this.snapAndWarpViolations();
-	this.createStencilTriangles();
+  this.sampleFields();
+  this.computeInterfaces();
+  this.generalizeTriangles();
+  this.snapAndWarpViolations();
+  this.createStencilTriangles();
 };
 
 window.Cleaver = {};
