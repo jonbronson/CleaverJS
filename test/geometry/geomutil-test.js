27321867ab55abaff817f6bbb92b8d51dd17c1e4
Jonathan Bronson <jonbronson@google.com>
2015-09-26 12:40:39 -0400

    line intersection test now handles parallel line case

--- test/geometry/geomutil-test.js
+++ test/geometry/geomutil-test.js
@@ -16,6 +16,17 @@ describe('GeomUtil Tests', function() {
       expect(result.ua).toBe(0.5);
       expect(result.ub).toBe(0.5);
     });
+
+    it('should handle parallel lines as intersections at infinity', function() {
+      var p1 = new Point(1,0);
+      var p2 = new Point(0,0);
+      var p3 = new Point(0,1);
+      var p4 = new Point(1,1);
+
+      var result = GeomUtil.computeLineIntersection(p1, p2, p3, p4);
+      expect(Math.abs(result.ua)).toBe(Number.POSITIVE_INFINITY);
+      expect(Math.abs(result.ub)).toBe(Number.POSITIVE_INFINITY);
+    });
   });
 
 });
