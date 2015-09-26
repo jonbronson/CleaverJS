33857ca867a9b0ef7113b4d93b21dff3ac1c3b13
Jonathan Bronson <jonbronson@google.com>
2015-09-26 12:34:39 -0400

    Added new working test for geomutil

--- test/geometry/geomutil-test.js
+++ test/geometry/geomutil-test.js
@@ -7,17 +7,14 @@ describe('GeomUtil Tests', function() {
 
   describe('computeLineIntersection()', function() {
     it('should interesct orthogonal unit vectors correctly', function() {
-      var p1 = new Point(0.5, 0.5);
-      /*
-
+      var p1 = new Point(0.5,   0);
       var p2 = new Point(0.5,   1);
       var p3 = new Point(  0, 0.5);
       var p4 = new Point(  1, 0.5);
-      */
 
-      //var result = GeomUtil.computeLineIntersection(p1, p2, p3, p4);
-      //expect(result.ua).toBe(0.5);
-      //expect(result.ub).toBe(0.5);
+      var result = GeomUtil.computeLineIntersection(p1, p2, p3, p4);
+      expect(result.ua).toBe(0.5);
+      expect(result.ub).toBe(0.5);
     });
   });
 
