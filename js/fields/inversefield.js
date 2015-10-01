3cf2e550b4dd92ae328ccfb7399d075b1fc68315
Jon Bronson <jonbronson@gmail.com>
2015-09-30 23:59:12 -0400

    adding jsdoc comments to inversefield class

--- js/fields/inversefield.js
+++ js/fields/inversefield.js
@@ -1,26 +1,50 @@
-var Vector = require('geometry/vector');
+/**
+ * @fileOverview This file defines the inverse field class
+ * @author Jonathan Bronson</a>
+ * @exports InverseField
+ */
 
 module.exports = (function(){ 
 
 'use strict';
 
+/**
+* Creates a new InverseField object
+* @class
+* @param {Field} field The field which this field is the inverse of.
+* @constructor
+* @alias InverseField
+* @extends Field
+*/
 var InverseField = function(field) {  
   this.field = field; 
   this.bounds = field.bounds;
 };
 
+/**
+ * @overide
+ */
 InverseField.prototype.valueAt = function(x, y) {
   return -1*this.field.valueAt(x,y);
 };
 
+/**
+ * @overide
+ */
 InverseField.prototype.getBounds = function() {
   return this.bounds;
 };
 
+/**
+ * @overide
+ */
 InverseField.prototype.getWidth = function() {
   return this.bounds.size.x;
 };
 
+/**
+ * @overide
+ */
 InverseField.prototype.getHeight = function() {
   return this.bounds.size.y;
 };
