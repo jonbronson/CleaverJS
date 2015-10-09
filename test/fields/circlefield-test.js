f714914e74303678bd89367adcfa539fd7d01bc8
Jonathan Bronson <jonbronson@google.com>
2015-10-08 22:34:17 -0400

    Adding test for InverseField

--- test/fields/circlefield-test.js
+++ test/fields/circlefield-test.js
@@ -20,6 +20,13 @@ describe('CircleField Tests', function() {
       expect(field.valueAt(51.192388,
                            72.192388)).toBeCloseTo(0, 6);
     });
+
+    it('should return negative outside the circle', function() {
+      var bounds = new Rect(0, 0, 100, 100);
+      var field = new CircleField(42, 63, 13, bounds);
+      expect(field.valueAt(5, 5)).toBeLessThan(0);
+
+    });
   });
 
 });
