8cb0bb1acda2e111fe38de419abb447e6f6e61b5
Jonathan Bronson <jonbronson@google.com>
2015-10-25 17:03:33 -0400

    Adding transformed field.

---
+++ js/fields/transformedfield.js
@@ -0,0 +1,59 @@
+/**
+ * @fileOverview This file defines the Transformed field class
+ * @author Jonathan Bronson</a>
+ * @exports TransformedField
+ */
+var Vector = require('../geometry/vector');
+
+module.exports = (function(){
+
+'use strict';
+
+/**
+* Creates a new TransformedField object
+* @class
+* @param {Field} field The field which this field transforms.
+* @param {Matrix} transform The transform matrix to apply to the input field.
+* @constructor
+* @alias TransformedField
+* @extends Field
+*/
+var TransformedField = function(field, transform, bounds) {
+  this.field = field;
+  this.transform = transform;
+  this.inverseTransform = transform.inverse();
+  this.bounds = bounds;
+};
+
+/**
+ * @overide
+ */
+TransformedField.prototype.valueAt = function(x, y) {
+  var transformedTo = this.inverseTransform.multiplyVector(new Vector(x,y));
+  return this.field.valueAt(transformedTo.x, transformedTo.y);
+};
+
+/**
+ * @overide
+ */
+TransformedField.prototype.getBounds = function() {
+  return this.bounds;
+};
+
+/**
+ * @overide
+ */
+TransformedField.prototype.getWidth = function() {
+  return this.bounds.size.x;
+};
+
+/**
+ * @overide
+ */
+TransformedField.prototype.getHeight = function() {
+  return this.bounds.size.y;
+};
+
+return TransformedField;
+
+}());
