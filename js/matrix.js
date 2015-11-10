4339e341eb3da5931d5fa03a2fe11d47876d0474
Jonathan Bronson <jonbronson@google.com>
2015-11-09 21:25:54 -0500

    adding missing jsdoc for matrix class. Cleaning up some code in place.

--- js/matrix.js
+++ js/matrix.js
@@ -1,3 +1,8 @@
+/**
+ * @fileOverview This file defines the Matrix class.
+ * @author Jonathan Bronson</a>
+ * @exports Matrix
+ */
 var Vector = require('geometry/vector');
 var Vector3 = require('geometry/vector3');
 
@@ -5,6 +10,21 @@ module.exports = (function(){
 
 'use strict';
 
+/**
+ * Creates a new Matrix object
+ * @class
+ * @param {number} a element [0][0]
+ * @param {number} b element [0][1]
+ * @param {number} c element [0][2]
+ * @param {number} d element [1][0]
+ * @param {number} e element [1][1]
+ * @param {number} f element [1][2]
+ * @param {number} g element [2][0]
+ * @param {number} h element [2][1]
+ * @param {number} i element [2][2]
+ * @constructor
+ * @alias Matrix
+ */
 var Matrix = function(a, b, c, d, e, f, g, h, i) {
   if (a == undefined) {
     var array = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
@@ -12,23 +32,30 @@ var Matrix = function(a, b, c, d, e, f, g, h, i) {
     var array = [[a, b, c], [d, e, f], [g, h, i]];
   }
 
-  var matrix = Object.create( Array.prototype );
-  matrix = (Array.apply( matrix, array ) || matrix);
-  Matrix.injectClassMethods( matrix );
+  var matrix = Object.create(Array.prototype);
+  matrix = Array.apply(matrix, array) || matrix;
+  Matrix.injectClassMethods_(matrix);
 
   return matrix;
 };
 
-Matrix.injectClassMethods = function(matrix){
+/**
+ * Add missing methods to the object instance.
+ * @returns {Matrix}
+ */
+Matrix.injectClassMethods_ = function(matrix){
   for (var method in Matrix.prototype){
     if (Matrix.prototype.hasOwnProperty(method)){
       matrix[method] = Matrix.prototype[method];
     }
   }
-  return(matrix);
+  return matrix;
 };
 
-
+/**
+ * Returns a readable version of the matrix.
+ * @returns {String}
+ */
 Matrix.prototype.toString = function() {
   var s = '[';
   for (var i=0; i < 3; i++) {
@@ -48,6 +75,10 @@ Matrix.prototype.toString = function() {
   return s;
 }
 
+/**
+ * Multiplies this matrix with the second one provided and returns the result.
+ * @returns {Matrix}
+ */
 Matrix.prototype.multiply = function(matrix) {
   var result = new Matrix(0, 0, 0, 0, 0, 0, 0, 0, 0);
   for (var i=0; i < 3; i++) {
@@ -60,12 +91,20 @@ Matrix.prototype.multiply = function(matrix) {
   return result;
 };
 
+/**
+ * Multiplies this matrix with the vector provided and returns the result.
+ * @returns {Vector}
+ */
 Matrix.prototype.multiplyVector = function(vector) {
   var vector3 = new Vector3(vector.x, vector.y, 1);
   var result = this.multiplyVector3(vector3);
   return new Vector(result.x / result.z, result.y / result.z);
 };
 
+/**
+ * Multiplies this matrix with the vector provided and returns the result.
+ * @returns {Vector3}
+ */
 Matrix.prototype.multiplyVector3 = function(vector) {
   var result = new Vector3();
   result.x = this[0][0]*vector.x + this[0][1]*vector.y + this[0][2]*vector.z;
@@ -74,6 +113,10 @@ Matrix.prototype.multiplyVector3 = function(vector) {
   return result;
 };
 
+/**
+ * Returns the inverse of this matrix.
+ * @returns {Matrix}
+ */
 Matrix.prototype.inverse = function() {
   var inverse = new Matrix();
   var determinant =  +this[0][0]*(this[1][1]*this[2][2]-this[2][1]*this[1][2])
@@ -92,6 +135,10 @@ Matrix.prototype.inverse = function() {
   return inverse;
 };
 
+/**
+ * Creates a new 2d rotation matrix, with a transform of theta radians.
+ * @returns {Matrix}
+ */
 Matrix.createRotation = function(theta) {
   var matrix = new Matrix();
   matrix[0][0] =  Math.cos(theta);
@@ -101,6 +148,10 @@ Matrix.createRotation = function(theta) {
   return matrix;
 };
 
+/**
+ * Creates a new 2d translation matrix using the given x and y coordinates.
+ * @returns {Matrix}
+ */
 Matrix.createTranslation = function(x, y) {
   var matrix = new Matrix();
   matrix[0][2] = x;
@@ -108,6 +159,10 @@ Matrix.createTranslation = function(x, y) {
   return matrix;
 };
 
+/**
+ * Creates a new 2d scale matrix to scale by sx horizontally and sy vertically.
+ * @returns {Matrix}
+ */
 Matrix.createScale = function(sx, sy) {
   var matrix = new Matrix();
   matrix[0][0] = sx;
