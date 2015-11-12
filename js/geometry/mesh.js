d8ceba634db4ca618ad7a11ad60661c5aff81c61
Jonathan Bronson <jonbronson@google.com>
2015-11-11 21:12:41 -0500

    fixing tabs in mesh class file

--- js/geometry/mesh.js
+++ js/geometry/mesh.js
@@ -33,25 +33,25 @@ var Mesh = function() {
  * @returns {Triangle} The newly created face.
  */
 Mesh.prototype.createFace = function(v1, v2, v3, material) {
-	if (!v1 || !v2 || !v3) {
-		console.log('problem!');
-	}
+  if (!v1 || !v2 || !v3) {
+    console.log('problem!');
+  }
 
-	var face = new Triangle(v1, v2, v3, material);
-	this.faces.push(face);
+  var face = new Triangle(v1, v2, v3, material);
+  this.faces.push(face);
 
-	if (v1.id === undefined) {
-		v1.id = this.verts.length;
-		this.verts.push(v1);
-	}
-	if (v2.id === undefined) {
-		v2.id = this.verts.length;
-		this.verts.push(v2);
-	}
-	if (v3.id === undefined) {
-		v3.id = this.verts.length;
-		this.verts.push(v3);
-	}
+  if (v1.id === undefined) {
+    v1.id = this.verts.length;
+    this.verts.push(v1);
+  }
+  if (v2.id === undefined) {
+    v2.id = this.verts.length;
+    this.verts.push(v2);
+  }
+  if (v3.id === undefined) {
+    v3.id = this.verts.length;
+    this.verts.push(v3);
+  }
 };
 
 /**
@@ -62,12 +62,12 @@ Mesh.prototype.createFace = function(v1, v2, v3, material) {
  * @returns {Array.<HalfEdge>} The two half edges.
  */
 Mesh.prototype.halfEdgeForVerts = function(v1, v2) {
-	var key = v1.pos.toString() + '|' + v2.pos.toString();
+  var key = v1.pos.toString() + '|' + v2.pos.toString();
   var halfEdge = this.halfEdges[key];
   if (!halfEdge) {
-  	halfEdge = new HalfEdge(v2);
-  	v1.halfEdges.push(halfEdge);
-  	this.halfEdges[key] = halfEdge;
+    halfEdge = new HalfEdge(v2);
+    v1.halfEdges.push(halfEdge);
+    this.halfEdges[key] = halfEdge;
   }
   return halfEdge;
 };
@@ -78,31 +78,31 @@ Mesh.prototype.halfEdgeForVerts = function(v1, v2) {
  */
 Mesh.prototype.buildAdjacency = function() {
 
-	// todo relace by using v[0]..v[2] instead of v1..v3
-	for (var f=0; f < this.faces.length; f++) {
-		var v1 = this.faces[f].v1;
-		var v2 = this.faces[f].v2;
-		var v3 = this.faces[f].v3;
+  // todo relace by using v[0]..v[2] instead of v1..v3
+  for (var f=0; f < this.faces.length; f++) {
+    var v1 = this.faces[f].v1;
+    var v2 = this.faces[f].v2;
+    var v3 = this.faces[f].v3;
 
-		// for (var e=0; e < 3; e++) {
-		this.faces[f].halfEdges[0] = this.halfEdgeForVerts(v1, v2);
-		this.faces[f].halfEdges[1] = this.halfEdgeForVerts(v2, v3);
-		this.faces[f].halfEdges[2] = this.halfEdgeForVerts(v3, v1);
+    // for (var e=0; e < 3; e++) {
+    this.faces[f].halfEdges[0] = this.halfEdgeForVerts(v1, v2);
+    this.faces[f].halfEdges[1] = this.halfEdgeForVerts(v2, v3);
+    this.faces[f].halfEdges[2] = this.halfEdgeForVerts(v3, v1);
 
-		for (var e=0; e < 3; e++)
-			this.faces[f].halfEdges[e].face = this.faces[f];
+    for (var e=0; e < 3; e++)
+      this.faces[f].halfEdges[e].face = this.faces[f];
 
-		this.faces[f].halfEdges[0].mate = this.halfEdgeForVerts(v2, v1);
-		this.faces[f].halfEdges[1].mate = this.halfEdgeForVerts(v3, v2);
-		this.faces[f].halfEdges[2].mate = this.halfEdgeForVerts(v1, v3);
-		this.faces[f].halfEdges[0].mate.mate = this.faces[f].halfEdges[0];
-		this.faces[f].halfEdges[1].mate.mate = this.faces[f].halfEdges[1];
-		this.faces[f].halfEdges[2].mate.mate = this.faces[f].halfEdges[2];
+    this.faces[f].halfEdges[0].mate = this.halfEdgeForVerts(v2, v1);
+    this.faces[f].halfEdges[1].mate = this.halfEdgeForVerts(v3, v2);
+    this.faces[f].halfEdges[2].mate = this.halfEdgeForVerts(v1, v3);
+    this.faces[f].halfEdges[0].mate.mate = this.faces[f].halfEdges[0];
+    this.faces[f].halfEdges[1].mate.mate = this.faces[f].halfEdges[1];
+    this.faces[f].halfEdges[2].mate.mate = this.faces[f].halfEdges[2];
 
-		this.faces[f].halfEdges[0].next = this.faces[f].halfEdges[1];
-		this.faces[f].halfEdges[1].next = this.faces[f].halfEdges[2];
-		this.faces[f].halfEdges[2].next = this.faces[f].halfEdges[0];
-	}
+    this.faces[f].halfEdges[0].next = this.faces[f].halfEdges[1];
+    this.faces[f].halfEdges[1].next = this.faces[f].halfEdges[2];
+    this.faces[f].halfEdges[2].next = this.faces[f].halfEdges[0];
+  }
 };
 
 /**
@@ -111,7 +111,7 @@ Mesh.prototype.buildAdjacency = function() {
  * @returns {Array.<HalfEdge>}
  */
 Mesh.prototype.getEdgesAroundVertex = function(vertex) {
-	return vertex.halfEdges;
+  return vertex.halfEdges;
 };
 
 /**
@@ -120,7 +120,7 @@ Mesh.prototype.getEdgesAroundVertex = function(vertex) {
  * @returns {Array.<Face>}
  */
 Mesh.prototype.getFacesAroundVertex = function(vertex) {
-	return vertex.faces
+  return vertex.faces
 };
 
 /**
@@ -129,18 +129,18 @@ Mesh.prototype.getFacesAroundVertex = function(vertex) {
  * @returns {Array.<Faces>}
  */
 Mesh.prototype.getFacesAroundEdge = function(edge) {
-	var faces = [];
+  var faces = [];
 
-	if (edge.face)
-		faces.push(edge.face);
-	if (edge.mate.face)
-		faces.push(edge.mate.face);
+  if (edge.face)
+    faces.push(edge.face);
+  if (edge.mate.face)
+    faces.push(edge.mate.face);
 
-	if (faces.length === 0) {
-		throw new Error ('Edge has no incident faces.');
-	}
+  if (faces.length === 0) {
+    throw new Error ('Edge has no incident faces.');
+  }
 
-	return faces;
+  return faces;
 };
 
 /**
@@ -148,7 +148,7 @@ Mesh.prototype.getFacesAroundEdge = function(edge) {
  * @returns {Array.<Faces>}
  */
 Mesh.prototype.getFaces = function() {
-	return this.faces;
+  return this.faces;
 }
 
 /**
@@ -156,8 +156,8 @@ Mesh.prototype.getFaces = function() {
  * @returns {Array.<Vertex}
  */
 Mesh.prototype.getVerticesAroundFace = function(triangle) {
-	var verts = [triangle.v1, triangle.v2, triangle.v3];
-	return verts;
+  var verts = [triangle.v1, triangle.v2, triangle.v3];
+  return verts;
 };
 
 /**
@@ -165,9 +165,9 @@ Mesh.prototype.getVerticesAroundFace = function(triangle) {
  * @returns {Array.<HalfEdge}
  */
 Mesh.prototype.getEdgesAroundFace = function(triangle) {
-	var edges = [triangle.halfEdges[0],
-							 triangle.halfEdges[1],
-							 triangle.halfEdges[2]];
+  var edges = [triangle.halfEdges[0],
+               triangle.halfEdges[1],
+               triangle.halfEdges[2]];
 	return edges;
 };
 
